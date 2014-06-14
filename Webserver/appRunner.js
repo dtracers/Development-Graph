e79a3eb9c09581065961f437e84533835314d826
dtracers <dtracers@gmail.com>
2014-06-14 13:25:53 -0500

    added chrome app functionality
    The program can now run as a chrome app that people can either get from
    the chrome app store or download the github and load it themselves

---
+++ Webserver/appRunner.js
@@ -0,0 +1,23 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = screen.availWidth;
+  var height = screen.availHeight;
+
+  chrome.app.window.create('index.html', {
+    id: "Developer-Graph",
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
+});
