07934d4332bae647ee9ab59e3fa76c84287e4f79
dtracers <dtracers@gmail.com>
2014-06-15 15:37:52 -0500

    Started a comment parser

---
+++ Webserver/js_parser/CommentParser.js
@@ -0,0 +1,20 @@
+/*
+* @Callback - describes what the callback should do, its possible conditions
+* @CallbackParam - a parameter given to the callback (these should be in the order that they are listed in the callback function)
+* @AbbreviatedDescription - a short one line description of the method.  (this is also inferred by the first sentence in the description)
+* @FullDescription - the full description of the method
+*
+*
+*
+* this will a couple of items to the document object
+*/
+
+/**
+ * @Class
+ * Goes through the comment section and performs comment parsing.
+ *
+ * This is where linking can happen and where other parts of a comment are added to the documentation object.
+ */
+function CommentParser() {
+	
+}
