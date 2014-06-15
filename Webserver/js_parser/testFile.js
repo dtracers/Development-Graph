835f204c3dc082719dfedf54c73e72142c31550e
dtracers <dtracers@gmail.com>
2014-06-15 15:36:29 -0500

    Added more examples for what it can parse

--- Webserver/js_parser/testFile.js
+++ Webserver/js_parser/testFile.js
@@ -1,10 +1,29 @@
 /**
- * global method
+ * @File
+ * This is a test file for the purpose of testing our js parser.
+ */
+
+/**
+ * global method1
  * @Method
  */
-function globalMethod() {
+function globalMethod1() {
 
 }
+
+/**
+ * global method2
+ * @Method
+ */
+function globalMethod2(arg1, arg2) {
+
+}
+
+/**
+ * global field
+ * @Field
+ */
+var globalTestField = 0;
 
 /**
  * global class
@@ -13,9 +32,16 @@ function globalMethod() {
 function TestClass() {
 	/**
 	 * @Field
+	 * class field version 1
 	 */
 	var testField = 0;
-	
+
+	/**
+	 * @Field
+	 * class field version 2
+	 */
+	this.testField2 = "hi";
+
 	 /**
 	  * class method version 1
 	  * @Method
@@ -28,7 +54,50 @@ function TestClass() {
 	  * class method version 2
 	  * @Method
 	  */
-	 this.testMethod2 = function() {
+	 this.testMethod2 = function(arg1, arg2) {
 
 	 };
+
+	 /**
+	  * nested class version 1
+	  * @Class
+	  */
+	 function NestedClass {
+
+		 /**
+		  * nested class method
+		  * @Method
+		  */
+		 function nestedMethod() {
+
+		 }
+	 }
+
+	 /**
+	  * @Method
+	  *
+	  * @TEMPClassStart
+	  * @TEMPClassName name
+	  * @TEMPClassEnd
+	  */
+	 this.getNestedClass = function() {
+		 return ;
+	 };
+}
+
+/**
+ * @Class
+ * A second global class for classing.
+ */
+function SecondGlobalClass {
+
+	/**
+	 * @StartFields
+	 */
+	var one;
+	var two = "two";
+	this.three = 3;
+	/**
+	 * @EndFields
+	 */
 }
