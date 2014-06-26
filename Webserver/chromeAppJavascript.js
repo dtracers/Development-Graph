64a37699ca9b29e41f2986e3d496eb7d165ef5af
dtracers <dtracers@gmail.com>
2014-06-26 00:43:59 -0700

    Added test code for file navigation

--- Webserver/chromeAppJavascript.js
+++ Webserver/chromeAppJavascript.js
@@ -1,4 +1,15 @@
 var f = new ProjectLoader();
 f.loadMainDirectory(function() {
 	console.log("Files are now ready to be used I hope!");
+
+	var navi = f.getProjectNavigator();
+	console.log(navi);
+	var files = navi.getFilesWithName("file");
+	console.log(files);
+	for (var i = 0; i < files.length; i++) {
+		console.log(files[i].toString());
+	}
+
+	var element = document.getElementById("fileText");
+	// */
 });
