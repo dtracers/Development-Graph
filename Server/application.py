2b075c679fbe418607a2bc02c16858a9d0f6f222
dtracers <dtracers@gmail.com>
2014-07-24 23:25:07 -0700

    Open webpage in browser

--- Server/application.py
+++ Server/application.py
@@ -8,16 +8,43 @@ import time
 import connection.server
 import BaseHTTPServer
 
-HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
+import webbrowser
+import threading
+
+HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS FOR REMOTE CONNECTION!!!
 PORT_NUMBER = 9000 # Maybe set this to 9000.
+CONST_REMOTE = False
+CONST_DEFAULT_PAGE = "/web/graph/graph.html"
+
+class DevelopmentGraph():
+
+    def __init__(self):
+        t = threading.Thread(target = self.openWebpage)
+        t.daemon = True
+        t.start()
+        self.createServer()
+
+    def createServer(self):
+        server_class = BaseHTTPServer.HTTPServer
+        httpd = server_class((HOST_NAME, PORT_NUMBER), connection.server.RequestHandler)
+        print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+        print 'starting server!'
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        httpd.server_close()
+        print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    
+    def openWebpage(self):
+        time.sleep(3)
+        print 'opening browser'
+        try:
+            webbrowser.open(HOST_NAME+ ":" + str(PORT_NUMBER) + CONST_DEFAULT_PAGE, 1)
+        except Exception:
+            print 'EXCEPTION'
+        print 'window opened'
+        
 
 if __name__ == '__main__':
-    server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), connection.server.RequestHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    DevelopmentGraph()
