a016e1be10956366af85ca4520e0867ce3fd4593
dtracers <dtracers@gmail.com>
2014-07-25 23:23:33 -0700

    Created a file Dialog Manager for python
    It hides the stupid tk thingy and just shows the actual dialog by itself

---
+++ Server/src/fileManager/fileDialogManager.py
@@ -0,0 +1,36 @@
+'''
+Created on Jul 25, 2014
+
+@author: gigemjt
+'''
+import Tkinter as tk
+from tkFileDialog import askdirectory
+import os
+from src.utilities import system
+class FileDialog():
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.pack_propagate()
+        self.root.withdraw()
+
+    def showDirectoryDialog(self):
+        """A blocking method that returns the directory path to a selected directory"""
+        self.root.update()
+        self.root.deiconify()
+        self.root.lift()
+        if system.isMac():
+            self.root.call('wm', 'attributes', '.', '-topmost', True)
+            self.root.after_idle(self.root.call, 'wm', 'attributes', '.', '-topmost', False)
+            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
+        self.root.withdraw()
+        self.name = askdirectory()
+        return self.name
+
+def hide(root):
+    root.withdraw()
+
+if __name__ == '__main__':
+    dialog = FileDialog()
+    name = dialog.showDirectoryDialog()
+    print name
