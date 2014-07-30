c0c7298b8d6af8fc923a7d3a3357798d2f8b0305
dtracers <dtracers@gmail.com>
2014-07-30 01:55:03 -0700

    Created a json writer
    Also added test ensuring that the json writer behaves correctly

---
+++ Server/java/src/utilities/json/StreamingJsonWriter.java
@@ -0,0 +1,222 @@
+package utilities.json;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ContentHandler;
+import org.json.simple.parser.ParseException;
+
+/**
+ * What this does is takes in the file and writes it out (to a temporary
+ * location in the same folder) If it comes across an item that needs to be
+ * added or replaced it will write that out and then continue on.
+ *
+ * This works on the specific principle that the item we are finding is either the first item in an object or not in the object at all
+ */
+
+public class StreamingJsonWriter extends BufferedReader implements ContentHandler {
+
+	Writer output; 
+	public StreamingJsonWriter(Reader in, Writer out) {
+		super(in);
+		this.output = out;
+	}
+
+	private int counter = 0;
+	private Map<String, JSONObject> objectList = new HashMap<String, JSONObject>();
+	private JSONObject currentObject = null;
+	private String key;
+	private String matchKey;
+	private boolean inObject = false;
+	private boolean tracking = false;
+	private boolean first = false;
+
+	public void setMatchKey(String matchKey) {
+		this.matchKey = matchKey;
+	}
+
+	@Override
+	public void startJSON() throws ParseException, IOException {
+		tracking = false;
+		first = true;
+	}
+
+	@Override
+	public void endJSON() throws ParseException, IOException {
+	}
+
+	@Override
+	public boolean endObjectEntry() throws ParseException, IOException {
+		return true;
+	}
+
+	@Override
+	public boolean startObjectEntry(String key) throws ParseException, IOException {
+		this.key = key;
+		if (key != null) {
+			if (!first) {
+				write(", ");
+			} else {
+				first = false;
+			}
+			if (!tracking) {
+				writeKey(key);
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean primitive(Object value) throws ParseException, IOException {
+		if (key != null) {
+			if (tracking) {
+				tracking = false; // no matter what we are no longer tracking
+				if (key.equals(matchKey)) {
+					if (checkValues(value)) {
+						key = null;
+						return true;
+					}
+				}
+				writeKey(key);
+			}
+			writeValue(value);
+			key = null;
+		} else {
+			if (!first) {
+				write(", ");
+			} else {
+				first = false;
+			}
+			writeValue(value);
+		}
+		return true;
+	}
+
+	private boolean checkValues(Object value) {
+		JSONObject obj = objectList.get(""+value);
+		if (obj != null) {
+			key = null;
+			inObject = true;
+			counter = 1;
+			tracking = false;
+			currentObject = obj;
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean startArray() throws ParseException, IOException {
+		if (tracking && !inObject) { // 
+			writeKey(key);
+		}
+		tracking = false;
+		// in the case of [5, [array]]
+		if (!first && key == null && !inObject) {
+			write(", ");
+		}
+		key = null;
+
+		if (inObject) {
+			counter ++;
+		} else {
+			write("[");
+		}
+		first = true;
+		return true;
+	}
+
+	@Override
+	public boolean endArray() throws ParseException, IOException {
+		if (inObject) {
+			counter --;
+		} else {
+			write("]");
+		}
+		first = false;
+		return true;
+	}
+
+	@Override
+	public boolean startObject() throws ParseException, IOException {
+		if (tracking && !inObject) { // 
+			writeKey(key);
+		}
+		if (!inObject) {
+			tracking = true;
+		}
+		// in the case of [5, {obj}]
+		if (!first && key == null && !inObject) {
+			write(", ");
+		}
+
+		key = null;
+		if (inObject) {
+			counter ++;
+		} else {
+			write("{");
+		}
+		first = true;
+		return true;
+	}
+
+	@Override
+	public boolean endObject() throws ParseException, IOException {
+		tracking = false;
+		if (inObject) {
+			counter -= 1;
+		}
+		if (inObject && counter <= 0) {
+			inObject = false;
+			writeReplacementObject(currentObject);
+			currentObject = null;
+		} else {
+			write("}");
+		}
+		first = false;
+		return true;
+	}
+
+	/**
+	 * Silently catches the exception thrown by writing.
+	 * @param str
+	 */
+	private void write(String str) {
+		try {
+			if (!inObject) {
+				output.write(str);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void writeReplacementObject(JSONObject currentObject) {
+		String result = currentObject.toJSONString();
+		write(result.substring(1));
+	}
+
+	private void writeKey(String key) {
+		String result = JSONObject.toString(key, null);
+		write(result.substring(0, result.indexOf(":")+1));
+	}
+
+	private void writeValue(Object value) {
+		String result = JSONObject.toString(null, value);
+		write(result.substring(result.indexOf(":") + 1));
+	}
+
+	public void setReplacementMap(Map<String, JSONObject> replacement) {
+		objectList = replacement;
+	}
+}
---
+++ Server/java/tst/JsonWriterTest.java
@@ -0,0 +1,411 @@
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONAware;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
+
+import utilities.json.StreamingJsonWriter;
+
+@SuppressWarnings("static-method")
+public class JsonWriterTest {
+	public static final String SIMPLE_REPLACEMENT = "{\"replace value!\":56}";
+	public static final String SIMPLE_REPLACEMENT2 = "{\"other replace value\":57}";
+	public static final String COMPLEX_REPLACEMENT = "{\"first\": 123, \"second\": [{\"k1\":{\"id\":\"id1\"}}, 4, {\"id\": 123}], \"third\": 789, \"id\": null}";
+	public static final String COMPLEX_REPLACEMENT2 = "{\"id\": 123, \"id\": [{\"k1\":{\"id\":\"id1\"}}, 4, {\"id\": 123}], \"id\": 789, \"id\": null}";
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 */
+	@Test
+	public void test1() throws IOException, ParseException { // level one!
+		String jsonText = "{\"first\":123}"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 */
+	@Test
+	public void test2() throws IOException, ParseException { // level two!
+		String jsonText = "{\"first\":123, \"second\":123}"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 */
+	@Test
+	public void test3() throws IOException, ParseException { // level three!
+		String jsonText = "[4, 5, 6]"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 */
+	@Test
+	public void test4() throws IOException, ParseException { // level four!
+		String jsonText = "[{\"first\":123}, 5, {\"second\":123}]"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 */
+	@Test
+	public void test5() throws IOException, ParseException { // level five!
+		String jsonText = "{\"first\":{\"second\":123}}"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 */
+	@Test
+	public void test6() throws IOException, ParseException { // level six
+		String jsonText = "{\"first\":123, \"second\":{\"third\":123}}"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * (same as 5 but with arrays)
+	 */
+	@Test
+	public void test7() throws IOException, ParseException { // level seven
+		String jsonText = "{\"first\":[123, 1234]}"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * (same as 6 but with arrays)
+	 */
+	@Test
+	public void test8() throws IOException, ParseException { // level eight
+		String jsonText = "{\"first\":123, \"second\":[123, 1234]}"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * (same as 5 and 7 but swapped arrays and objects)
+	 */
+	@Test
+	public void test9() throws IOException, ParseException { // level nine
+		String jsonText = "[{\"second\":123, \"first\":123}]"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * (same as 6 and 8 but swapped arrays and objects)
+	 */
+	@Test
+	public void test10() throws IOException, ParseException { // level ten
+		String jsonText = "[123, {\"second\":123}]"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * empty object test
+	 */
+	@Test
+	public void test11() throws IOException, ParseException { // level eleven
+		String jsonText = "[{}, []]"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * empty object test
+	 */
+	@Test
+	public void test12() throws IOException, ParseException { // level twelve
+		String jsonText = "{\"empty\":{}, \"array\":[]}"; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * empty object test
+	 */
+	@Test
+	public void test13() throws IOException, ParseException { // level final
+		String jsonText = COMPLEX_REPLACEMENT; 
+		Reader source = new StringReader(jsonText);
+		String result = createResult(source);
+		jsonEquals(COMPLEX_REPLACEMENT, result);
+	}
+
+	@SuppressWarnings("resource")
+	public static String createResult(Reader source) throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		BufferedReader read = new BufferedReader(source);
+		StringWriter str = new StringWriter();
+		StreamingJsonWriter finder = new StreamingJsonWriter(read, str);
+		parser.parse(finder, finder, true);
+		return str.toString();
+	}
+
+	/*
+	 * ALL TEST BELOW TEST REPLACMENT!
+	 */
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * This one passes if the object is NOT replaced.
+	 * Specifically The second item does not trigger a replacement
+	 */
+	@Test
+	public void test1ReplacmentNOT() throws IOException, ParseException { // stage 1.5 level 1
+		String jsonText = "{\"first\":123, \"second\":124}";
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("124", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "second");
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * This one passes if the object is NOT replaced.
+	 * Specifically if the key is valid but the value does not match.
+	 */
+	@Test
+	public void test2ReplacmentNOT() throws IOException, ParseException { // stage 1.5 level 2
+		String jsonText = "{\"first\":123, \"second\":124}";
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("125", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "second");
+		jsonEquals(jsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Single replacement
+	 */
+	@Test
+	public void test1Replacment() throws IOException, ParseException { // stage 2 level 1
+		String jsonText = "{\"first\":123}";
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("123", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "first");
+		jsonEquals(SIMPLE_REPLACEMENT, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Single replacement first item in array
+	 */
+	@Test
+	public void test2Replacment() throws IOException, ParseException { // stage 2 level 2
+		String jsonText = "[{\"first\":123}, 5, {\"second\":123}]";
+		String replacedJsonText = "[" + SIMPLE_REPLACEMENT + ", 5, {\"second\":123}]";
+
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("123", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "first");
+		jsonEquals(replacedJsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Single replacement second item in array
+	 */
+	@Test
+	public void test3Replacment() throws IOException, ParseException { // stage 2 level 3
+		String jsonText = "[{\"first\":123}, 5, {\"second\":123}]";
+		String replacedJsonText = "[{\"first\":123}, 5, " + SIMPLE_REPLACEMENT + "]";
+
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("123", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "second");
+		jsonEquals(replacedJsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Single replacement second item in array.  Same as test 4 but uses complex replacement instead.
+	 */
+	@Test
+	public void test4Replacment() throws IOException, ParseException { // stage 2 level 4
+		String jsonText = "[{\"first\":123}, 5, {\"second\":123}]";
+		String replacedJsonText = "[{\"first\":123}, 5, " + COMPLEX_REPLACEMENT + "]";
+
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("123", createObject(COMPLEX_REPLACEMENT));
+		String result = createResult(source, map, "second");
+		jsonEquals(replacedJsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Single replacement second item in array.  Replaces a completex replacment with a simple one
+	 */
+	@Test
+	public void test5Replacment() throws IOException, ParseException { // stage 2 level 5
+		String jsonText = "[{\"ignore\":123}, 5, " + COMPLEX_REPLACEMENT + "]";
+		String replacedJsonText = "[{\"ignore\":123}, 5, " + SIMPLE_REPLACEMENT  + "]";
+
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("123", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "first");
+		jsonEquals(replacedJsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Single replacement second item in array.  Deeper object embedding
+	 */
+	@Test
+	public void test6Replacment() throws IOException, ParseException { // stage 2 level 4
+		String jsonText = "[{\"first\":123}, 5, {\"second\":{\"id\":1}}]";
+		String replacedJsonText = "[{\"first\":123}, 5, {\"second\":" + SIMPLE_REPLACEMENT + "}]";
+
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("1", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "id");
+		jsonEquals(replacedJsonText, result);
+	}
+
+	/*
+	 * Everything below uses multiple replacement
+	 */
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Single Replacment
+	 */
+	@Test
+	public void test1NOTMultiReplacment() throws IOException, ParseException { // stage 2.5 level 5
+		String jsonText = "[{\"ignore\":123}, 5, " + COMPLEX_REPLACEMENT2 + "]";
+		String replacedJsonText = "[{\"ignore\":123}, 5, " + SIMPLE_REPLACEMENT  + "]";
+
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("123", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "id");
+		jsonEquals(replacedJsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Double Replacment
+	 */
+	@Test
+	public void test2MultiSameReplacment() throws IOException, ParseException { // stage 2.5 level 5
+		String jsonText = "[{\"first\":123}, 5, " + COMPLEX_REPLACEMENT + "]";
+		String replacedJsonText = "[" + SIMPLE_REPLACEMENT + ", 5, " + SIMPLE_REPLACEMENT  + "]";
+
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("123", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "first");
+		jsonEquals(replacedJsonText, result);
+	}
+
+	/**
+	 * Passes if the input string is the same as the output string.
+	 * Replacment with different ID
+	 */
+	@Test
+	public void test3MultiReplacment() throws IOException, ParseException { // stage 2.5 level 5
+		String jsonText = "[{\"id\":124}, 5, {\"id\":123}]";
+		String replacedJsonText = "[" + SIMPLE_REPLACEMENT2 + ", 5, " + SIMPLE_REPLACEMENT  + "]";
+
+		Reader source = new StringReader(jsonText);
+		Map<String, JSONObject> map = createMap();
+		map.put("124", createObject(SIMPLE_REPLACEMENT2));
+		map.put("123", createObject(SIMPLE_REPLACEMENT));
+		String result = createResult(source, map, "id");
+		jsonEquals(replacedJsonText, result);
+	}
+
+	public Map<String, JSONObject> createMap() {
+		return new HashMap<String, JSONObject>();
+	}
+
+	public JSONObject createObject(String result) {
+		JSONParser parser = new JSONParser();
+		try {
+			return (JSONObject) parser.parse(result);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@SuppressWarnings("resource")
+	public static String createResult(Reader source, Map<String, JSONObject> replacement, String key) throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		BufferedReader read = new BufferedReader(source);
+		StringWriter str = new StringWriter();
+		StreamingJsonWriter finder = new StreamingJsonWriter(read, str);
+		finder.setMatchKey(key);
+		finder.setReplacementMap(replacement);
+		parser.parse(finder, finder, true);
+		return str.toString();
+	}
+
+	private void jsonEquals(String expected, String result) {
+		JSONParser parser = new JSONParser();
+		JSONAware  expectedObj = null;
+		JSONAware  resultObj = null;
+		try {
+			expectedObj = (JSONAware) parser.parse(expected);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			resultObj = (JSONAware) parser.parse(result);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(expectedObj, resultObj);
+	}
+}
