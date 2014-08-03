009c9c58e2befd30cecdb44a3e922d7f0fe2b850
dtracers <dtracers@gmail.com>
2014-08-02 17:02:07 -0700

    Updated the server to open the web page by itself

--- Server/java/src/application/Main.java
+++ Server/java/src/application/Main.java
@@ -1,19 +1,22 @@
 package application;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import connection.Server;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws URISyntaxException {
 		System.out.println();
 		try {
 			System.out.println("Running");
 			new Server("localHost", 9000).start();
+			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:9000/web/index.html"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
             System.in.read();
         } catch (Throwable ignored) {
