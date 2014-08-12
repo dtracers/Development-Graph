3ef8a5494bf7c3621b37a84a4ea19bf03c8e0a24
dtracers <dtracers@gmail.com>
2014-08-11 22:52:48 -0700

    Added a much better text file

--- FakeTestProject/src/Test.java
+++ FakeTestProject/src/Test.java
@@ -1,10 +1,22 @@
-public class test {
-
-	pulic static void main(String args[]) {
-		System.out.println(“FEATURE 1”);
-	}
-
-	public static void feature2() {
-		// does nothing but it has a bug
+/**
+ * This is about <code>ClassName</code>.
+ * {@link com.yourCompany.aPackage.Interface}
+ * @author author
+ * @deprecated use <code>OtherClass</code>
+ */
+public class ClassName<E> extends AnyClass implements InterfaceName<String> {
+	enum Color { RED, GREEN, BLUE };
+	/* This comment may span multiple lines. */
+	static Object staticField;
+	// This comment may span only this line
+	private E field;
+	private AbstractClassName field2;
+	// TASK: refactor
+	@SuppressWarnings(value="all")
+	public int foo(Integer parameter) {
+		abstractMethod(inheritedField);
+		int local= 42*hashCode();
+		staticMethod();
+		return bar(local) + parameter;
 	}
 }
