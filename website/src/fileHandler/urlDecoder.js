f6b2a62e948906b0fced680aa485c107e3b06844
dtracers <dtracers@gmail.com>
2014-08-11 22:53:44 -0700

    Added a file that contains utilities for url decoding

---
+++ website/src/fileHandler/urlDecoder.js
@@ -0,0 +1,39 @@
+/**
+ * Looks at the url in the document and returns the project name.
+ * @returns {string} the project name embedded in the current page's url
+ */
+function getProjectFromUrl() {
+	var url = document.URL;
+	var webStart = 'web-';
+	url = url.substring(url.indexOf(webStart) + webStart.length);
+	return url.substring(0, url.indexOf('/'));
+}
+
+function getDataDirectoryAsUrl() {
+	return "/project-" + getProjectFromUrl() + "/.dgd";
+}
+
+function getSourceDirectoryAsUrl() {
+	return "/project-" + getProjectFromUrl();
+}
+
+/**
+ * @Method
+ * @returns {string} A url referencing a webpage with the project name embedded with it.
+ */
+function getWebsitePathAsUrl() {
+	return "/web-" + getProjectFromUrl() + "/src";
+}
+
+/**
+ * Returns the file extension of a file given a url
+ * @param url
+ * @returns
+ */
+function getExtensionFromUrl(url) {
+	if (url.indexOf(".") < 0) {
+		return undefined;
+	}
+	var extensionIndex = url.lastIndexOf(".");
+	return url.substring(extensionIndex + 1);
+}
