8a7330dd9a053c88931d44fa90175bde2fff96a0
dtracers <dtracers@gmail.com>
2014-08-13 00:46:33 -0700

    Updated file creation and reading test
    Updated to support new server system.
    The new tests on the website also use the new utility system.

--- Server/java/tst/website/FileUtilsTest.java
+++ Server/java/tst/website/FileUtilsTest.java
@@ -15,30 +15,45 @@ import projectManagment.Project;
 import projectManagment.ProjectManager;
 import connection.Server;
 
+@SuppressWarnings("static-method")
 public class FileUtilsTest {
 	private static final String TEST_PROJECT_NAME = "TestProject";
 	private static final File TEST_DIRECTORY = new File(Server.WORKING_DIR.substring(0, Server.WORKING_DIR.indexOf("Server")) + "FakeTestProject/");
 	private static final String SERVER_DIRECTORY = Server.WORKING_DIR;
 	private static final String TEST_DATA_URL = "/project-" + TEST_PROJECT_NAME + "/.dgd/";
 	private static final long TIMEOUT_TIME = 1000 * 60 * 5; // 5 minutes
-	private static Server serv;
+	private static TestServer serv;
 
 	@BeforeClass
-	public static void createFakeProjectAndServer() throws IOException, URISyntaxException {
+	public static void createFakeProjectAndServer() throws IOException {
 		Project p = new Project(TEST_PROJECT_NAME, TEST_DIRECTORY);
 		ProjectManager.getInstance().addProject(p);
 
 		System.out.println("Running server");
-		serv = new TestServer("localHost", 9000);
+		serv = new TestServer("localHost", 9001); // Specific port for this test!
 		serv.start();
 	}
 
 	@Test
-	public void test() throws IOException, InterruptedException, URISyntaxException {
-		java.awt.Desktop.getDesktop().browse(new URI("http://localhost:9000/web/tst/fileHandler/fileUtilsTest.html?noglobals"));
-		((TestServer) serv).pause(TIMEOUT_TIME);
-		assertTrue(((TestServer) serv).testEnded());
-		assertEquals(0, ((TestServer) serv).getNumberOfFailedTest());
+	public void testReading() throws IOException, InterruptedException, URISyntaxException {
+		String identifier = "testReading";
+		String testUrl = serv.getTestUrl(identifier, "fileHandler/fileCreationAndReadingTest.html?noglobals");
+		java.awt.Desktop.getDesktop().browse(new URI(testUrl));
+		serv.pause(TIMEOUT_TIME, identifier);
+		assertTrue(serv.testEnded(identifier));
+		assertEquals(0, serv.getNumberOfFailedTest());
+	}
+
+	@Test
+	public void testWriting() throws IOException, InterruptedException, URISyntaxException {
+		String identifier = "testWriting";
+		String testUrl = serv.getTestUrl(identifier, "fileHandler/fileWritingTest.html?noglobals");
+		java.awt.Desktop.getDesktop().browse(new URI(testUrl));
+		//serv.pause(TIMEOUT_TIME, identifier);
+		assertTrue(serv.testEnded(identifier));
+		assertEquals(0, serv.getNumberOfFailedTest());
+
+		// now we actaully confirm that the writing has taken place.
 	}
 
 	@AfterClass
