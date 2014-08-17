252b66978233d51d4135ed843501ff377a68fc4e
dtracers <dtracers@gmail.com>
2014-08-16 23:41:43 -0700

    Created a code file attacher.
    This attaches code/docs/tests to an actual feature

---
+++ website/src/codemanager/codeFileAttacher.js
@@ -0,0 +1,74 @@
+/**
+ * 
+ * @param evt
+ */
+function handleDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+/**
+ * @Class
+ */
+CODE_ATTACHER = new (function() {
+	
+	// grab type and feautriId myself
+
+	var type = getParameterByName("type");
+	var featureId = getParameterByName("feature");
+	var featureFileObject;
+
+	/**
+	 * @Method
+	 * @param file {String|File}
+	 * Using the url it adds the file to the list of files for a given feature
+	 */
+	var fileHandler = function(file) {
+		console.log(file);
+		var myFile = new AbstractedFile(file);
+		var fileName = "";
+		if (myFile.isDirectory()) {
+			fileName = myFile.getName();
+		} else {
+			fileName = myFile.getFullName();
+		}
+		fileObject = myFile.createJson();
+		featureFileObject.files.push(fileObject);
+		writeData();
+	};
+
+	/**
+	 * @Method
+	 * @param type the type of loading to perform (if it is source, doc, test)
+	 * @returns {Function}
+	 */
+	this.createAttachResponse = function(asFile) {
+		var SOURCE_TYPE = "source";
+		var DOC_TYPE = "doc";
+		var TEST_TYPE = "test";
+		if (asFile) {
+			return fileHandler;
+		} else {
+			return urlHandler;
+		}
+	};
+
+	/**
+	 * @Method
+	 * Loads the specific feature that we want to attach files to.
+	 */
+	this.loadFeatureData = function() {
+		var fileGrabber = new AbstractedFile(undefined, getDataDirectoryAsUrl() + "/" + type + "?" + featureId);
+		fileGrabber.readFileAsJson(function(json) {
+			featureFileObject = json[0];
+			console.log(featureFileObject);
+			alert(featureFileObject);
+		});
+	};
+
+	function writeData() {
+		var fileGrabber = new AbstractedFile(undefined, getDataDirectoryAsUrl() + "/" + type + "?" + featureId);
+		fielGrabber.writeJsonToFile(featureFileObject);
+	}
+})();
