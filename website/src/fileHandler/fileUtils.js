15c06966cba4f5c4f388dc1b06a4205f1fbeab61
dtracers <dtracers@gmail.com>
2014-08-23 19:06:26 -0700

    Added ends with to file utils

--- website/src/fileHandler/fileUtils.js
+++ website/src/fileHandler/fileUtils.js
@@ -1,3 +1,9 @@
+if (typeof String.prototype.endsWith !== 'function') {
+    String.prototype.endsWith = function(suffix) {
+        return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+}
+
 /**
  * @Class
  */
